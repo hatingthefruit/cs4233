7fdec4dbd01086e352e4a82eb95a6eb665ec2886
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2021-10-05 10:58:53 -0500

    Finished hw

--- hw2/HW2.m
+++ hw2/HW2.m
@@ -9,7 +9,7 @@ clear
 % clear commmand window
 clc;
 
-%% this cell is for testing myNWalign (Q1) 
+%% this cell is for testing myNWalign (Q1)
 % complete the myNWalign function and run this cell to test
 
 seq1 = 'atctcta';
@@ -17,7 +17,7 @@ seq2 = 'tcata';
 
 % calls the myNWalign function that you define, and output the score as well as alignment
 % matrix in the command window
-[score_myNW, matrix_mynw]=myNWalign(seq1, seq2, 1, -1, -1)
+[score_myNW, matrix_mynw] = myNWalign(seq1, seq2, 1, -1, -1)
 
 % FYI, plot the alignment matrix returned from above func call
 plotAliMatrix(seq1, seq2, matrix_mynw, 'global alignment matrix');
@@ -25,14 +25,13 @@ plotAliMatrix(seq1, seq2, matrix_mynw, 'global alignment matrix');
 % FYI, uncomment below to see result from nwalign in MATLAB bioinformatics
 % toolbox. The matrix plotted by nwalign is a transpose of myNWalign; hence
 % I used seq2 vs seq1. Also, it does not show the first row and first column of
-% the matrix returned by myNWalgin. In addition, the two figures look different as they 
-% use different value to color mappings. You can change the colormap 
-% by clicking "edit" => "colormap" or use the colormap function ("help colormap"). 
-
+% the matrix returned by myNWalgin. In addition, the two figures look different as they
+% use different value to color mappings. You can change the colormap
+% by clicking "edit" => "colormap" or use the colormap function ("help colormap").
 
 %[score_nw, ali_nw] = nwalign(seq2, seq1, 'showscore', true, 'alphabet', 'nt', 'scoringmatrix', eye(4)*2-1, 'gapopen', 1, 'extendgap', 1)
 
-%% this cell is for testing mySWalign (Q2) 
+%% this cell is for testing mySWalign (Q2)
 % complete the mySWalign function and run this cell to test
 
 seq1 = 'atctcta';
@@ -41,7 +40,7 @@ seq2 = 'tcact';
 % calls the mySWalign function that you define, and output the score as well as alignment
 % matrix in the command window
 
-[score, matrix]=mySWalign(seq1, seq2, 1, -1, -1)
+[score, matrix] = mySWalign(seq1, seq2, 1, -1, -1)
 
 % FYI, plot the alignment matrix returned from above func call
 plotAliMatrix(seq1, seq2, matrix, 'local alignment matrix');
@@ -49,7 +48,7 @@ plotAliMatrix(seq1, seq2, matrix, 'local alignment matrix');
 % FYI, uncomment below to see result from nwalign in MATLAB bioinformatics
 % toolbox. The matrix plotted by swalign is a transpose of mySWalign; hence
 % I used seq2 vs seq1. Also, it does not show the first row and first column of
-% the matrix returned by mySWalgin. 
+% the matrix returned by mySWalgin.
 
 % [score_nwalign, alignment] = swalign(seq2, seq1, 'showscore', true, 'alphabet', 'nt', 'scoringmatrix', eye(4)*2-1, 'gapopen', 1, 'extendgap', 1)
 
@@ -59,24 +58,27 @@ seqL = 200;
 nRepeat = 1000;
 rand_score = zeros(1, nRepeat);
 
-
 % 3.1
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Insert your code here to compute the alignment scores for 1000 pairs of
 % random sequences and save in array rand_score
+last_seq = randseq(seqL);
 
+for seqN = 1:nRepeat
+    curr_seq = randseq(seqL);
+    [rand_score(seqN), ~] = mySWalign(last_seq, curr_seq, 1, -1, -100);
+    last_seq = curr_seq;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % display statistics
-disp('Alignment statistics from 1000 pairs of random sequences') 
+disp('Alignment statistics from 1000 pairs of random sequences')
 disp(sprintf('Min score: %.3g', min(rand_score)))
 disp(sprintf('Max score: %.3g', max(rand_score)))
 disp(sprintf('Mean score: %.3g', mean(rand_score)))
 disp(sprintf('Median score: %.3g', median(rand_score)))
-
 
 % show the histogram
 figure();
@@ -87,12 +89,11 @@ title('Alignment statistics');
 
 % 3.2
 
+thresholds = 11:13; % scoreing thresholds to calcluate probabilities
 
-thresholds=11:13; % scoreing thresholds to calcluate probabilities
-
-counts_observed = zeros(1,length(thresholds));  % how many alignment scores are >= each threshold?
-p_theory = zeros(1,length(thresholds)); % use extreme value distribution to estimate number of seg
-p_theory_divided_by_k = zeros(1,length(thresholds)); % p_theory, but without k
+counts_observed = zeros(1, length(thresholds)); % how many alignment scores are >= each threshold?
+p_theory = zeros(1, length(thresholds)); % use extreme value distribution to estimate number of seg
+p_theory_divided_by_k = zeros(1, length(thresholds)); % p_theory, but without k
 
 lamda = log(3); % lamda is solved for match = 1, mismatch = -1.
 
@@ -100,14 +101,23 @@ lamda = log(3); % lamda is solved for match = 1, mismatch = -1.
 % Insert your code here to compute counts_observed, p_theory, and
 % p_theory_divided_by_k
 
+for num = 1:nRepeat
+
+    if rand_score(num) >= 11
+        index = rand_score(num) - 11 + 1;
+        counts_observed(index:3) = counts_observed(index:3) + 1;
+    end
+end
+
 % counts: how many values in rand_score are >= threshold?
 % p_theory: assuming k = 0.1, what is the expected number of segments to
 % have score >= threshold
+k_th = 0.1;
+p_theory = (k_th * seqL * seqL)*exp(-lamda*thresholds);
 % p_theory_divided_by_k: ignoring k (or assuming k = 1), what is the expected number of segments to
 % have score >= threshold
-
+p_theory_divided_by_k = (seqL * seqL)*exp(-lamda*thresholds);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 p_observed = counts_observed / nRepeat;
 
@@ -122,27 +132,33 @@ hold off
 
 k = mean(p_observed ./ p_theory_divided_by_k)
 
-
 %% 3.3
 
 seq1 = randseq(seqL); % rand seq of length seqL
 mutationRate = 0.3; % mutation rate
-mask = randperm(seqL, round(seqL*mutationRate)); % choose random positions to mutate
+mask = randperm(seqL, round(seqL * mutationRate)); % choose random positions to mutate
 seq2 = seq1; % seq2 is a copy of seq1
 seq2(mask) = randseq(length(mask)); %selected locations are mutated to random chars (may be the same char as in seq1)
 
 score = mySWalign(seq1, seq2, 1, -1, -100);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Replace the following two lines below with the correct formulas to compute the e-value 
+% Replace the following two lines below with the correct formulas to compute the e-value
 % and p-value with the k value estimated from 3.2. Formula for p-value is
 % on slide page 172. If the p-value turns out to be 0, use e-value as
 % p-value instead.
 % Do NOT add semincolon at the end of the line, so that the result will be
 % displayed in command window.
 
-eval = 1
 
-pval = 1
+eval = (k * seqL * seqL) * exp(-lamda*score)
+
+pval = 1 - exp(-eval);
+
+if pval == 0
+    pval = eval
+else
+    pval
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- hw2/myNWalign.m
+++ hw2/myNWalign.m
@@ -1,30 +1,72 @@
 function [score, matrix] = myNWalign(seq1, seq2, match, mismatch, gap)
-% This function performs global alignment between two DNA sequences using
-% the Needleman-Wunsch algorithm (Lecture slide page 55. No trackback
-% needed.)
-% Input:
-%   seq1 and seq2 must be DNA characters (case insensititive)
-%   match score should be positive 
-%   mismatch and gap score should be negative (or zero)
-% Returns: 
-%   score: score for the best global alignment
-%   matrix: dynamic programming matrix, whose dimension is (1+length(seq1), 1+length(seq2))
-%   note that index in MATLAB starts from 1. As a result, matrix(1, 1)
-%   corresponds to F(0, 0) in lecture slides 
+    % This function performs global alignment between two DNA sequences using
+    % the Needleman-Wunsch algorithm (Lecture slide page 55. No trackback
+    % needed.)
+    % Input:
+    %   seq1 and seq2 must be DNA characters (case insensititive)
+    %   match score should be positive
+    %   mismatch and gap score should be negative (or zero)
+    % Returns:
+    %   score: score for the best global alignment
+    %   matrix: dynamic programming matrix, whose dimension is (1+length(seq1), 1+length(seq2))
+    %   note that index in MATLAB starts from 1. As a result, matrix(1, 1)
+    %   corresponds to F(0, 0) in lecture slides
 
+    seq1 = upper(seq1);
+    seq2 = upper(seq2);
 
-seq1 = upper(seq1);
-seq2 = upper(seq2);
-if (~all(ismember([seq1,seq2], 'ACGT')))
-    error('input seq contains non-DNA chars');
-end
+    if (~all(ismember([seq1, seq2], 'ACGT')))
+        error('input seq contains non-DNA chars');
+    end
 
-% initialization
-score = 0;
-m = length(seq1);
-n = length(seq2);
-matrix = zeros(m+1, n+1);
+    % initialization
+    score = 0;
+    m = length(seq1);
+    n = length(seq2);
+    matrix = zeros(m + 1, n + 1);
+    seq1 = strcat('\ ', seq1);
+    seq2 = strcat('\ ', seq2);
+    for i = 1:m + 1
+        matrix(i, 1) = (i - 1) * gap;
+    end
 
+    for j = 1:n + 1
+        matrix(1, j) = (j - 1) * gap;
+    end
 
-% provide your own code to compute matrix and score(no return statement
-% needed in MATLAB).
+    % provide your own code to compute matrix and score(no return statement
+    % needed in MATLAB).
+
+    for i = 2:m + 1
+        for j = 2:n + 1
+            if i == 1 & j == 1
+                break
+            elseif i == 1 & j ~= 1
+                left = matrix(i, j - 1);
+                up = left;
+                up_left = left;
+            elseif j == 1
+                up = matrix(i - 1, j);
+                left = up;
+                up_left = up;
+            else
+                up = matrix(i - 1, j);
+                left = matrix(i, j - 1);
+                up_left = matrix(i - 1, j - 1);
+            end
+            if seq1(i) == seq2(j)
+                curr = match;
+            else
+                curr = mismatch;
+            end
+
+            opt_list = [up + gap, left + gap, up_left + curr];
+            matrix(i, j) = max(opt_list);
+
+            if matrix(i, j) > score
+                score = matrix(i, j);
+            end
+
+        end
+
+    end
